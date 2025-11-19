61a8219cb46776a162e31765367632ed78dc7f6c
EfeUCLL <efe.okan@student.ucll.be>
2025-11-19 14:48:16 +0100

    correct project structure

---
+++ front-end/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front-end',
+  webDir: 'dist'
+};
+
+export default config;
