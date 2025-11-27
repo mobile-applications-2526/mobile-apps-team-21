210030b64b98d8a566f8e8e669d086e58fb76ea8
Görkem Ertugrullu <gorkem@MacBook-Air-van-Gorkem.local>
2025-11-27 21:21:49 +0100

    v1 working ios

--- back-end/src/main/java/be/ucll/EatUp_Team21/security/CorsProperties.java
+++ back-end/src/main/java/be/ucll/EatUp_Team21/security/CorsProperties.java
@@ -11,7 +11,10 @@ public record CorsProperties(
                 "http://localhost:*",
                 "http://127.0.0.1:*",
                 "http://10.0.2.2:*",
-                "http://192.168.*:*"
+                "http://192.168.*:*",
+                // Capacitor/Ionic native webview origins
+                "capacitor://localhost",
+                "ionic://localhost"
         }) List<String> allowedOrigins
 ) {
 }
